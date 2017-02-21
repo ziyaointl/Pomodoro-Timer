2eb59e2141f2d02f6b4402c8c3b298ed50b04758
Ziyao Zhang <ziyaointl@gmail.com>
2017-02-21 10:00:05 +0800

    Imported bulma

--- src/App.vue
+++ src/App.vue
@@ -16,5 +16,18 @@ export default {
 </script>
 
 <style lang="scss">
+    @import "css/bulma/utilities/_all.sass";
+    @import "css/bulma/base/_all.sass";
+    @import "css/bulma/layout/_all.sass";
+    @import "css/bulma/elements/_all.sass";
+    @import "css/bulma/grid/_all.sass";
 
+    .title {
+        margin-top: 20px;
+        margin-left: 40px;
+    }
+
+    .time {
+        font-size: 30px;
+    }
 </style>
